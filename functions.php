e7655e7310eed4b8842b11d12fe7eca5a6f845c9
Ivan Didenko <32499861+ivandidenkoweb@users.noreply.github.com>
2020-04-01 19:28:02 +0300

    Настраиваем заголовок и логотип сайта

--- functions.php
+++ functions.php
@@ -8,11 +8,13 @@ function childhood_styles() {
     // Подключение доп стилей
     // wp_enqueue_style( 'header-style', get_template_directory_uri() . '/assets/styles/main.min.css');
     // wp_enqueue_style( 'animate-style', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css');
-}
+};
 
 function childhood_scripts() {
     // Подключение основного скрипта
     wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
-}
+};
+
+add_theme_support( 'custom-logo' );
 
 ?>
