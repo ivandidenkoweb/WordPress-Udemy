620cff091f0c8d03acaebc07b2d230607ade0c89
Ivan Didenko <32499861+ivandidenkoweb@users.noreply.github.com>
2020-04-03 13:25:46 +0300

    Настройка форм
    Использую Contact Form 7

--- functions.php
+++ functions.php
@@ -13,9 +13,23 @@ function childhood_styles() {
 function childhood_scripts() {
     // Подключение основного скрипта
     wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
+    // Подключение новой версии jquery
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+    wp_enqueue_script('jquery');
 };
 
 add_theme_support( 'custom-logo' );
 add_theme_support( 'post-thumbnails');
+
+// function my_acf_google_map_api( $api ){
+	
+// 	$api['key'] = 'AIzaSyBiBHdboWdB62MASJq8fi17likDB9-hSoc'; // Ваш ключ Google API
+	
+// 	return $api;
+	
+// }
+
+// add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
 
 ?>
